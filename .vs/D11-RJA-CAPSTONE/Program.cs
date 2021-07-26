e80b94e47916bca5a03688676cdd80a1fa74fffd
casaro <casaro@HMS-D11-02>
2021-07-26 12:43:49 -0400

    POG

---
+++ .vs/D11-RJA-CAPSTONE/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace D11_RJA_CAPSTONE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string[] Pkeys = new string[] { "We", "e", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+            string[] Nkeys = new string[] { "" };
+            string[] array = new string[12];
+
+
+
+            Console.Write("Hello, this program was made for the 2021-RJA Capstone Project by the BSD hosted at hunt.\n\n-Made by Kaung\n_________________________________________________________________________________________ \n");
+            Console.Write("\nPlease select your reading level:\n\n1(Beginner)\n\n2(Intermediate)\n\n3(Advanced)\n\n\"Beginner = Reading Levels A1 to I1, Intermediate = Reading Levels E2 to Q1, Advanced = Reading Levels M3 to Z.\"\n\nType corrosponding number here: ");
+            string Levelanswer = (Console.ReadLine());
+            if (Levelanswer == "1")
+            {
+                //Testing if option works
+                Console.ForegroundColor = ConsoleColor.Green;
+                Thread.Sleep(1000);
+
+                Console.WriteLine("\nHello World \"If it's green, you're good!\"");
+                Console.ResetColor();
+                Thread.Sleep(5000);
+                Console.Clear();
+                CountdownCuzImLazy();
+            }
+            if (Levelanswer == "2")
+            {
+                //Also tesing if option works
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Thread.Sleep(1000);
+
+                Console.WriteLine("\nGreetings Land \"If it's Yellow, you're good!\"");
+                Console.ResetColor();
+                Thread.Sleep(5000);
+                Console.Clear();
+                CountdownCuzImLazy();
+            }
+            if (Levelanswer == "3")
+            {
+                //Testing again "This is getting annoying"
+                Console.ForegroundColor = ConsoleColor.Red;
+                Thread.Sleep(1000);
+
+                Console.WriteLine("\nYou have chosen (Advanced)\nAcknowledgements Planet \"If it's red, then you're good!\"");
+                Console.ResetColor();
+                Thread.Sleep(5000);
+                Console.Clear();
+                CountdownCuzImLazy();
+            }
+            Console.Read();
+        }
+        static void CountdownCuzImLazy()
+        {
+            Console.WriteLine("Starting in: 3");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Starting in: 2");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Starting in: 1");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Int32[] RandAssetnum = new int[] { };
+            
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Thread.Sleep(500);
+Console.WriteLine("Loading Assets /184");
+            Console.ResetColor();
+            Console.Clear();
+        }
+        public static void Placebo2()
+        {
+            
+
+
+
+
+
+        }
+
+
+
+
+
+    }   
+        
+}
